c01ab1775669e61d917a325e68ce132ffaaa2856
DanZ <dvmonteiro0701@gmail.com>
2025-12-02 13:57:04 -0300

    Switch to relative base path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import inject from '@rollup/plugin-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hanbotdb/',
+  base: './',
   plugins: [
     vue(),
     inject({
