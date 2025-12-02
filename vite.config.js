babbd57f83d5efe324ddf435bf8fc6e1d0f12349
DanZ <dvmonteiro0701@gmail.com>
2025-12-02 13:52:26 -0300

    Fix deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import inject from '@rollup/plugin-inject';
-import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/hanbotdb/',
   plugins: [
     vue(),
     inject({
       $: ['jquery', '*'],
       jQuery: 'jquery',
     }),
-    viteSingleFile()
   ],
   resolve: {
     alias: {
